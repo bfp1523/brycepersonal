c20a4a9b23b76b61b22dc2923819101ac4d53a71
Bryce Peterson <bfp@umich.edu>
2015-04-26 12:54:25 -0400

    Added new templates

--- views.py
+++ views.py
@@ -6,3 +6,11 @@ import string
 @app.route('/')
 def home():
 	return render_template('home.html')
+
+@app.route('/resume/')
+def resume():
+	return render_template('resume.html')
+
+@app.route('/contact/')
+def contact():
+	return render_template('contact.html')
